32765f11e01044a5213c2b41b0168882c458e5c4
Igor Arecippo <iarecippo@gmail.com>
2023-08-10 17:45:23 -0300

    feat: implements scrape_news function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,8 +38,22 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+
+    news = {
+        'url': selector.css('link[rel=canonical]::attr(href)').get(),
+        'title': selector.css('h1.entry-title::text').get().strip(),
+        'timestamp': selector.css('li.meta-date::text').get().strip(),
+        'writer': selector.css(
+            'li.meta-author span.author a::text').get().strip(),
+        'reading_time': int(selector.css(
+            '.meta-reading-time::text').re_first(r'\d+')),
+        'summary': "".join(selector.css(
+            'div.entry-content > p:first-of-type *::text').getall()).strip(),
+        'category': selector.css('span.label::text').get().strip()
+    }
+
+    return news
 
 
 # Requisito 5
