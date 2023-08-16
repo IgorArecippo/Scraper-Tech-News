d92b9019d3d006753ee6426e62ad88cc73bcaaa4
Igor Arecippo <iarecippo@gmail.com>
2023-08-16 15:34:48 -0300

    feat: implements func search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 import re
 from tech_news.database import db
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,8 +16,18 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        parsed_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    formatted_date = parsed_date.strftime("%d/%m/%Y")
+    news_found = db.news.find(
+        {"timestamp": formatted_date},
+        {"_id": 0, "title": 1, "url": 1}
+    )
+    results = [(news["title"], news["url"]) for news in news_found]
+    return results
 
 
 # Requisito 9
