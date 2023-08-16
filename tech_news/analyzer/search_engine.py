f28ab7ac7d60e6dfe30fc1cc5a23872a206f62b1
Igor Arecippo <iarecippo@gmail.com>
2023-08-16 09:50:29 -0300

    feat: add function search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,16 @@
+import re
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    case_insensitive_title = re.compile(title, re.IGNORECASE)
+    news_found = db.news.find(
+        {"title": {"$regex": case_insensitive_title}},
+        {"_id": 0, "title": 1, "url": 1}
+    )
+    results = [(news["title"], news["url"]) for news in news_found]
+    return results
 
 
 # Requisito 8
