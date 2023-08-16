1b175a987c408547d2fc77fc6895b9537d11500f
Igor Arecippo <iarecippo@gmail.com>
2023-08-16 15:50:13 -0300

    feat: implements func top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = db.news.aggregate([
+        {
+            "$group": {
+                "_id": "$category",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1, "_id": 1}
+        }
+    ])
+
+    top_categories = [item["_id"] for item in news]
+    return top_categories[:5]
